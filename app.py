8d813cf8cdb37cec1f0a854be489c56887a27933
Anhelina Shulha <anhelina.shulha@gmail.com>
2018-02-28 12:34:44 -0800

    can't fetch data. need a premium access

--- app.py
+++ app.py
@@ -9,23 +9,25 @@ app = Flask(__name__)
 
 API_KEY = 'zC992yeEkw5VTye5PFJY'
 
-@app.route('/apple')
-def apple():
-	r = requests.get('https://www.quandl.com/api/v3/datasets/EOD/AAPL.json?api_key=' + API_KEY)
-	# json_obj = r.text
-	json_obj = r.json()
-
-	dataset = json_obj['dataset']
-	return render_template('index.html', dataset=dataset)
-	# return json_obj
-
-@app.route('/')
-def index():
-	r = requests.get('https://www.quandl.com/api/v3/datasets.json?database_code=EOD&sort_by=id')
+@app.route('/EOD/', methods=['POST'])
+@app.route('/EOD/<dataset_code>', methods=['POST'])
+def info(dataset_code='APPL'):
+	# database_code = request.form('name');
+	r = requests.get('https://www.quandl.com/api/v3/datasets/EOD/' + dataset_code + '.json?api_key=' + API_KEY)
 	json_obj = r.text
 	# json_obj = r.json()
 
-	# tmp = json_obj['datasets']
+	# dataset = json_obj['dataset']
+	# return render_template('index.html', dataset=dataset)
+	return json_obj
+
+@app.route('/')
+def index():
+	r = requests.get('https://www.quandl.com/api/v3/datasets.json?api_key=' + API_KEY + '&database_code=EOD&sort_by=id&per_page=1000&meta_data=true')
+	# json_obj = r.text
+	json_obj = r.json()
+
+	datasets = json_obj['datasets']
 	#
 	# arr_names = []
 	# for i in tmp:
@@ -36,8 +38,8 @@ def index():
 	# 	arr_names.append(i.name)
 	# return arr_names
 	# print names
-	return json_obj
-	# return render_template('home.html', arr=arr_names)
+	# return json_obj
+	return render_template('home.html', datasets=datasets)
 
 if (__name__ == '__main__'):
 	app.run(host='', port=7777, debug=True)
